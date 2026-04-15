65d53136c8bb78aebf3383a81539048510f937cd
Alex-chturmovik-SPARTA <korot.alex12@gmail.com>
2026-04-15 18:13:06 +0700

    Add files via upload

--- game_informatics_71.py
+++ game_informatics_71.py
@@ -1,5 +1,5 @@
 import pygame
-import random
+import os
 
 WIDTH = 800
 HEIGHT = 650
@@ -12,29 +12,44 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
-
+game_folder = os.path.dirname(__file__)
+player_img = pygame.image.load(os.path.join(game_folder, 'car.png'))
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((50, 50))
-        self.image.fill(RED)
+        self.orig_image = player_img
+        self.image = self.orig_image
         self.rect = self.image.get_rect()
         self.rect.center = (WIDTH / 2, HEIGHT / 2)
 
     def update(self):
         self.speedx = 0
         self.speedy = 0
+        self.pv = 0
+        pov = 0
         keystate = pygame.key.get_pressed()
-        if keystate[pygame.K_LEFT]:
-            self.speedx = -8
-        if keystate[pygame.K_RIGHT]:
-            self.speedx = 8
         if keystate[pygame.K_UP]:
-            self.speedy = -8
+            self.speedy = -10
+            if keystate[pygame.K_LEFT]:
+                self.pv = self.pv + 2
+                self.speedx = self.speedx - 2
+            if keystate[pygame.K_RIGHT]:
+                self.pv = self.pv - 2
+                self.speedx = self.speedx + 2
         if keystate[pygame.K_DOWN]:
             self.speedy = 8
+            if keystate[pygame.K_LEFT]:
+                pov = pov - 2
+                self.pv = pov
+                self.speedx = self.speedx - 2
+            if keystate[pygame.K_RIGHT]:
+                pov = pov + 2
+                self.pv = pov
+                self.speedx = self.speedx + 2
         self.rect.x += self.speedx
         self.rect.y += self.speedy
+        self.image = pygame.transform.rotate(self.orig_image, self.pv)
+        self.rect = self.image.get_rect(center=self.rect.center)
         if self.rect.right < 0:
             self.rect.right = WIDTH
         if self.rect.left > WIDTH:
@@ -49,7 +64,7 @@ class Player(pygame.sprite.Sprite):
 pygame.init()
 pygame.mixer.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("My Game")
+pygame.display.set_caption("Medical Game")
 clock = pygame.time.Clock()
 all_sprites = pygame.sprite.Group()
 player = Player()
@@ -62,13 +77,13 @@ while running:
     clock.tick(FPS)
     # Ввод процесса (события)
     for event in pygame.event.get():
-        # check for closing window
+        # закрытие через крестик
         if event.type == pygame.QUIT:
             running = False
-
     # Обновление
     all_sprites.update()
-
+    player.update()
+    screen.blit(player.image, player.rect)
     # Рендеринг
     screen.fill(GREEN)
     all_sprites.draw(screen)
